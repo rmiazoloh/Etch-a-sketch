07c9d72ad86825221fbf4b4c3a7ec8e1cf058aba
Lerudit <rmiazoloh@gmail.com>
2025-09-03 22:14:21 +0200

    Add darkermode functionnality

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
-// constants
+// CONSTANTS
 const DEFAULT_GRID_SIZE = 24;
 
+// GLOBAL
+let nbSquareColored = 0;
+
 const elements = {
-    square : null,
-    gridContainer : document.querySelector('.grid-container'),
-    buttonSize : document.querySelector('.btn-size'),
-    sizeDialog : document.getElementById('size-dialog'),
-    sizeInput : document.getElementById('size-input'),
-    confirmButton : document.getElementById('confirm-size'),
-    gridNotification : document.querySelector('.grid-notifications')
+    square: null,
+    gridContainer: document.querySelector('.grid-container'),
+    buttonSize: document.querySelector('.btn-size'),
+    sizeDialog: document.getElementById('size-dialog'),
+    sizeInput: document.getElementById('size-input'),
+    confirmButton: document.getElementById('confirm-size'),
+    gridNotification: document.querySelector('.grid-notifications'),
+    clearGrid: document.getElementById('clear'),
+    randomColor: document.getElementById('random-color-input'),
+    darkerModeInput: document.getElementById('darker-color-input')
 }
 
 const elementsProperties = {
-    gridContainerWidth : parseFloat(window.getComputedStyle(elements.gridContainer).width)
+    gridContainerWidth: parseFloat(window.getComputedStyle(elements.gridContainer).width)
 };
 
-function isValidValue(value){
-    if (value && !isNaN(value)){
+function isValidValue(value) {
+    if (value && !isNaN(value)) {
         return true;
     }
 }
 
 function handleGridSizeUpdate(event) {
     const newSizeValue = elements.sizeInput.value;
-    
+
     if (isValidValue(newSizeValue) && newSizeValue != '') {
         const parsedValue = parseInt(newSizeValue);
         const limitedValue = Math.min(parsedValue, 100);
-        
+
         elements.gridContainer.innerHTML = ''; // Clear the existing grid
         createGrid(limitedValue);
         elements.sizeInput.value = '';
-        
+
         elements.gridNotification.classList.add('success');
         elements.gridNotification.innerHTML = 'Grid size updated !';
     } else {
@@ -40,31 +46,97 @@ function handleGridSizeUpdate(event) {
     }
 }
 
-function setupListenerDrawEvents(){
+function setupListenerDrawEvents() {
+
     let mouseDown = false;
-    document.addEventListener('mousedown', () => {
-        mouseDown = true;
+    document.addEventListener('mousedown', (e) => {
+        if (e.button === 0) { // Clic gauche uniquement
+            mouseDown = true;
+        }
     })
-    document.addEventListener('mouseup', () => {
-        mouseDown = false;
+    document.addEventListener('mouseup', (e) => {
+        if (e.button === 0) { // Clic gauche uniquement
+            mouseDown = false;
+            nbSquareColored = 0;
+        }
     })
+
+    let isRandomColor = false;
+    elements.randomColor.addEventListener('click', () => {
+        if (elements.randomColor.checked) {
+            isRandomColor = true;
+        } else {
+            isRandomColor = false;
+        }
+    })
+
+    let isDarkerMode = false;
+    elements.darkerModeInput.addEventListener('click', () => {
+        if (elements.darkerModeInput.checked) {
+            isDarkerMode = true;
+        } else {
+            isDarkerMode = false;
+        }
+    });
 
     elements.square = document.querySelectorAll('.square');
     elements.square.forEach(square => square.addEventListener('mousedown', (event) => {
-        event.target.classList.add('colored-background');
+        if(isDarkerMode){
+            event.target.classList.add('colored-background-light');
+        }else{
+            event.target.classList.add('colored-background');
+            if (isRandomColor) {
+                event.target.style.backgroundColor = generateRandomColor();
+            }
+        }
+
+
     }));
 
     elements.square.forEach(square => square.addEventListener('mouseover', (event) => {
         if (mouseDown) {
             event.target.classList.add('colored-background');
+            if (isRandomColor) {
+                event.target.style.backgroundColor = generateRandomColor();
+            }
+            if (isDarkerMode) {
+                let element = event.target;
+                element.style.opacity = 0;
+                event.target.style.opacity = addOpacity(element);
+            }
+            nbSquareColored++;
         }
     }));
 
     elements.confirmButton.addEventListener("click", handleGridSizeUpdate);
+    elements.clearGrid.addEventListener('click', () => {
+        elements.gridContainer.innerHTML = '';
+        createGrid(DEFAULT_GRID_SIZE);
+        elements.randomColor.checked = false;
+        elements.darkerModeInput.checked = false;
+        elements.gridNotification.innerHTML = 'Grid cleaned !';
+        elements.gridNotification.classList.add('infos');
+    })
+}
+
+function generateRandomColor() {
+    const hex = '0123456789ABCDEF'
+    let output = '#';
+    for (let i = 0; i < 6; i++) {
+        output += hex.charAt(Math.floor(Math.random() * hex.length));
+    }
+    return output;
+}
+
+function addOpacity(element) {
+    // Convertir en nombre (parseFloat) et augmenter de 10%
+    const opacity = window.getComputedStyle(element).opacity;
+    let newOpacity = Math.min(parseFloat(opacity) + (0.1 * nbSquareColored), 1);
+    return newOpacity;
+
 }
 
 function createGrid(size) {
-    //console.log(size);
     const numberSquare = size * size;
     for (let i = 0; i < numberSquare; i++) {
         const square = document.createElement('div');
@@ -72,12 +144,11 @@ function createGrid(size) {
         square.style.width = elementsProperties.gridContainerWidth / size + 'px';
         square.style.height = elementsProperties.gridContainerWidth / size + 'px';
         elements.gridContainer.appendChild(square);
-        //console.log(square.style.width);
+
     }
     setupListenerDrawEvents();
 }
 createGrid(DEFAULT_GRID_SIZE);
-
 
 
 
